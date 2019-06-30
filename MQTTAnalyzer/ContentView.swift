2b6f95bd6aa10703fab3727a1abfc08c484ab389
Philipp Arndt <2f.mail@gmx.de>
2019-06-30 12:01:46 +0200

    Initial Commit

---
+++ MQTTAnalyzer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MQTTAnalyzer
+//
+//  Created by Philipp Arndt on 2019-06-30.
+//  Copyright © 2019 Philipp Arndt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
