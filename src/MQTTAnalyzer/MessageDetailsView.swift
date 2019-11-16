515b2de1280d0c279eed160f61cfea6af0e0d010
Philipp Arndt <2f.mail@gmx.de>
2019-11-16 19:07:22 +0100

    initial

--- src/MQTTAnalyzer/MessageDetailsView.swift
+++ src/MQTTAnalyzer/MessageDetailsView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import Highlightr
 
 struct MessageDetailsView : View {
     let message: Message
@@ -39,16 +40,77 @@ struct MessageDetailsView : View {
                 }
                 Section(header: Text("Message")) {
                     VStack {
-                        Text(message.isJson() ? message.prettyJson() : message.data)
+                        if (message.isJson()) {
+                            JSONView(message: message.prettyJson())
+                        }
+                        else {
+                            Text(message.data)
                             .lineLimit(nil)
                             .padding(20)
                             .font(.system(.body, design: .monospaced))
+                        }
+                        
                     }
                 }
             }
-            .listStyle(GroupedListStyle())
         }
     }
+}
+
+//struct TextView: UIViewRepresentable {
+//    @Binding var text: String
+//
+//    func makeUIView(context: Context) -> UITextView {
+//        return UITextView()
+//    }
+//
+//    func updateUIView(_ uiView: UITextView, context: Context) {
+//        uiView.text = text
+//    }
+//}
+//
+//struct ContentView : View {
+//    @State var text = ""
+//
+//    var body: some View {
+//        TextView(text: $text)
+//            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+//    }
+//}
+
+struct JSONView : View {
+    let message: String
+    @State private var textViewHeight: CGFloat = 400
+    
+    var body: some View {
+        TextWithAttributedString(textViewHeight: self.$textViewHeight,
+                                 attributedString: highlightText(json: self.message))
+//        .frame(minWidth: 0, maxWidth: .infinity, minHeight: size(json: self.message), maxHeight: .infinity)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: textViewHeight, maxHeight: .infinity)
+//        .frame(height: size(json: self.message))
+        .lineLimit(nil)
+        .padding(20)
+    }
+    
+    private func highlightText(json message: String) -> NSAttributedString {
+        let highlightr = Highlightr()!
+        highlightr.setTheme(to: "paraiso-dark")
+
+        return highlightr.highlight(message, as: "json")!
+    }
+    
+//    private func size(json message: String) -> CGFloat {
+//        let highlightr = Highlightr()!
+//        highlightr.setTheme(to: "paraiso-dark")
+//        let string =  highlightr.highlight(message, as: "json")!
+//
+//        let view = ViewWithLabel(frame:CGRect.zero)
+//        view.setString(string)
+//        view.scale()
+//        print(view.height)
+//
+//        return view.height * 2
+//    }
 }
 
 #if DEBUG
---
+++ src/MQTTAnalyzer/text/TextWithAttributedString.swift
@@ -0,0 +1,70 @@
+//
+//  TextWithAttributedString.swift
+//  SwiftUIAttributedStrings
+//
+//  Created by Gualtiero Frigerio on 20/08/2019.
+//  Copyright © 2019 Gualtiero Frigerio. All rights reserved.
+//
+import SwiftUI
+
+class ViewWithLabel : UIView {
+    private var label = UILabel()
+    var height : CGFloat = 0
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        self.addSubview(label)
+        label.numberOfLines = 0
+        label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        if traitCollection.userInterfaceStyle == .light {
+            print("Light mode")
+        } else {
+            print("Dark mode")
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if traitCollection.userInterfaceStyle == .light {
+            print("Light mode")
+        } else {
+            print("Dark mode")
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setString(_ attributedString:NSAttributedString) {
+//        self.label.numberOfLines = 0
+        self.label.attributedText = attributedString
+//        self.label.sizeToFit()
+//        self.height = self.label.frame.height
+    }
+    
+    func scale() {
+        self.label.layoutSubviews()
+        self.label.sizeToFit()
+        self.height = self.label.frame.height
+    }
+}
+
+struct TextWithAttributedString: UIViewRepresentable {
+    @Binding var textViewHeight: CGFloat
+    var attributedString:NSAttributedString
+    
+    func makeUIView(context: Context) -> ViewWithLabel {
+        return ViewWithLabel(frame:CGRect.zero)
+    }
+    
+    func updateUIView(_ uiView: ViewWithLabel, context: UIViewRepresentableContext<TextWithAttributedString>) {
+        uiView.setString(attributedString)
+        
+//        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) {
+//            print("Update heigt")
+//            self.textViewHeight = uiView.height
+//        }
+
+    }
+}
