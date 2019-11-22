74acf4d9b47d8e53a6980e24cd4099a643ba158c
Philipp Arndt <2f.mail@gmx.de>
2019-11-22 22:09:33 +0100

    clean-up

---
+++ src/MQTTAnalyzer/views/host/EditHostFormModalView.swift
@@ -0,0 +1,67 @@
+//
+//  EditHostFormModalView.swift
+//  MQTTAnalyzer
+//
+//  Created by Philipp Arndt on 2019-11-22.
+//  Copyright © 2019 Philipp Arndt. All rights reserved.
+//
+
+import SwiftUI
+
+
+// MARK: Edit Host
+struct EditHostFormModalView : View {
+    @Binding var isPresented : Bool
+    let root : RootModel
+    var hosts: HostsModel
+    let original : Host
+    
+    @State var host : HostFormModel
+    @State private var auth : Bool = false
+    
+    var body: some View {
+        NavigationView {
+            EditHostFormView(host: $host, auth: $auth)
+                .font(.caption)
+                .navigationBarTitle(Text("Edit host"))
+                .navigationBarItems(
+                    leading: Button(action: cancel) { Text("Cancel") },
+                    trailing: Button(action: save) { Text("Save") }
+            )
+        }
+    }
+    
+    func save() {
+        original.alias = host.alias
+        original.hostname = host.hostname
+        original.qos = host.qos
+        original.auth = self.auth
+        original.port = Int32(host.port) ?? 1883
+        original.topic = host.topic
+        
+        if (self.auth) {
+            original.username = host.username
+            original.password = host.password
+        }
+        else {
+            original.username = ""
+            original.password = ""
+        }
+        
+        root.persistence.update(original)
+
+        self.isPresented = false
+        clear()
+    }
+    
+    func cancel() {
+        self.isPresented = false
+        clear()
+    }
+    
+    
+    func clear() {
+        host = HostFormModel()
+        auth = false
+    }
+}
---
+++ src/MQTTAnalyzer/views/host/NewHostFormModalView.swift
@@ -0,0 +1,64 @@
+//
+//  NewHostFormModalView.swift
+//  MQTTAnalyzer
+//
+//  Created by Philipp Arndt on 2019-11-22.
+//  Copyright © 2019 Philipp Arndt. All rights reserved.
+//
+
+import SwiftUI
+
+// MARK: Create Host
+struct NewHostFormModalView : View {
+    @Binding var isPresented : Bool
+    let root : RootModel
+    var hosts: HostsModel
+    
+    @State private var host : HostFormModel = HostFormModel()
+    @State private var auth : Bool = false
+    
+    var body: some View {
+        NavigationView {
+            EditHostFormView(host: $host, auth: $auth)
+                .font(.caption)
+                .navigationBarTitle(Text("New host"))
+                .navigationBarItems(
+                    leading: Button(action: cancel) { Text("Cancel") },
+                    trailing: Button(action: save) { Text("Save") }
+            )
+        }
+    }
+    
+    func save() {
+        let newHost =  Host()
+        newHost.alias = host.alias
+        newHost.hostname = host.hostname
+        newHost.qos = host.qos
+        newHost.auth = self.auth
+        newHost.port = Int32(host.port) ?? 1883
+        newHost.topic = host.topic
+        
+        if (self.auth) {
+            newHost.username = host.username
+            newHost.password = host.password
+        }
+        
+        hosts.hosts.append(newHost)
+        
+        root.persistence.create(newHost)
+        
+        self.isPresented = false
+        clear()
+    }
+    
+    func cancel() {
+        self.isPresented = false
+        clear()
+    }
+    
+    
+    func clear() {
+        host = HostFormModel()
+        auth = false
+    }
+}
