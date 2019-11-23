d4968bd5e6923437a653537d23b935f8209b336e
Philipp Arndt <2f.mail@gmx.de>
2019-11-23 08:10:59 +0100

    move examples to separate file

--- src/MQTTAnalyzer/model/persistence/HostModelPersistence.swift
+++ src/MQTTAnalyzer/model/persistence/HostModelPersistence.swift
@@ -14,9 +14,11 @@ import RxSwift
 class HostsModelPersistence {
     let bag = DisposeBag()
     let model : HostsModel
+    let realm : Realm
     
     init(model: HostsModel) {
         self.model = model
+        self.realm = try! Realm()
     }
     
     func create(_ host: Host) {
@@ -29,7 +31,6 @@ class HostsModelPersistence {
     }
         
     func update(_ host: Host) {
-        let realm = try! Realm()
         let settings = realm.objects(HostSetting.self)
             .filter("id = %@", host.id)
         
@@ -47,7 +48,6 @@ class HostsModelPersistence {
     }
     
     func delete(_ host: Host) {
-        let realm = try! Realm()
         let settings = realm.objects(HostSetting.self)
             .filter("id = %@", host.id)
         
@@ -58,48 +58,8 @@ class HostsModelPersistence {
         }
     }
     
-    private func initializeExampleData(realm: Realm) {
-        let example1 = HostSetting()
-        example1.id = "example.test.mosquitto.org.1"
-        example1.alias = "Water levels"
-        example1.auth = false
-        example1.hostname = "test.mosquitto.org"
-        example1.username = ""
-        example1.password = ""
-        example1.port = 1883
-        example1.qos = 0
-        example1.topic = "de.wsv/#"
-        
-        let example2 = HostSetting()
-        example2.id = "example.test.mosquitto.org.2"
-        example2.alias = "Revspace sensors"
-        example2.auth = false
-        example2.hostname = "test.mosquitto.org"
-        example2.username = ""
-        example2.password = ""
-        example2.port = 1883
-        example2.qos = 0
-        example2.topic = "revspace/sensors/#"
-        
-        createIfNotPresent(setting: example1, realm: realm)
-        createIfNotPresent(setting: example2, realm: realm)
-    }
-    
-    private func createIfNotPresent(setting: HostSetting, realm: Realm) {
-        let settings = realm.objects(HostSetting.self)
-             .filter("id = %@", setting.id)
-        
-        if (settings.isEmpty) {
-            try! realm.write {
-                realm.add(setting)
-            }
-        }
-    }
-    
     func load() {
-        let realm = try! Realm()
-        
-        initializeExampleData(realm: realm)
+        HostSettingExamples.inititalize(realm: realm)
         
         let settings = realm.objects(HostSetting.self)
         
