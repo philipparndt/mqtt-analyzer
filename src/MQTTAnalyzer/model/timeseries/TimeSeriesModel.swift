f8970e7f74815255439d321c8fb720983f81e40b
Philipp Arndt <2f.mail@gmx.de>
2020-01-05 10:02:32 +0100

    remove unused code

--- src/MQTTAnalyzer/model/timeseries/TimeSeriesModel.swift
+++ src/MQTTAnalyzer/model/timeseries/TimeSeriesModel.swift
@@ -61,49 +61,5 @@ class DiagramPath: Hashable, Identifiable {
 }
 
 class MTimeSeriesModel {
-
 	var values: [MTimeSeriesValue] = []
-	
-	func getMeanValue(amount segements: Int, in rangeMinutes: Int, to end: Date) -> [MTimeSeriesMeanValue] {
-		// MARK: TODO Subscript for mean values in range
-		
-		let distance = TimeInterval((rangeMinutes * 60) / segements)
-		let start = end.advanced(by: -TimeInterval((rangeMinutes * 60)))
-		
-		let inRange = valuesInRange(values: values, from: start, to: end)
-		
-		var current = end
-		var result = [MTimeSeriesMeanValue]()
-		for _ in 1...segements {
-			let next = current.advanced(by: -distance)
-			
-			let valuesInCurrentRange = valuesInRange(values: inRange, from: next, to: current)
-			let value = buildMean(value: valuesInCurrentRange)
-			
-			current = next
-			result.insert(value, at: 0)
-		}
-		
-		return result
-	}
-	
-	func valuesInRange(values: [MTimeSeriesValue], from start: Date, to end: Date) -> [MTimeSeriesValue] {
-		return values.filter {
-			return $0.timestamp.distance(to: start) <= 0
-				&& $0.timestamp.distance(to: end) >= 0
-		}
-	}
-	
-	func buildMean(value range: [MTimeSeriesValue]) -> MTimeSeriesMeanValue {
-		if range.isEmpty {
-			return MTimeSeriesMeanValue(meanValue: nil)
-		}
-		else {
-			let total = range
-				.map { $0.value.intValue }
-				.reduce(0, +)
-			
-			return MTimeSeriesMeanValue(meanValue: total / range.count)
-		}
-	}
 }
