ef0fa82da1ba2f4a85d8b198c21d31af5c717da3
Philipp Arndt <2f.mail@gmx.de>
2020-01-05 09:23:59 +0100

    about view scrollable

--- src/MQTTAnalyzer/views/about/AboutView.swift
+++ src/MQTTAnalyzer/views/about/AboutView.swift
@@ -13,79 +13,15 @@ struct AboutView: View {
 	@Binding var isPresented: Bool
 	
 	var body: some View {
-		VStack(alignment: .leading) {
-			Group {
-				HStack {
-					Image("About")
-						.resizable()
-						.frame(width: 50.0, height: 50.0)
-						.cornerRadius(10)
-						.shadow(radius: 10)
-						.padding(.trailing)
-					
-					VStack(alignment: .leading) {
-						Text("MQTTAnalyzer")
-							.font(.title)
-	
-						LinkButtonView(text: "© 2020 Philipp Arndt", url: "https://github.com/philipparndt")
-							.font(.caption)
-							.foregroundColor(.secondary)
-					}
+		VStack {
+			AboutTitleView().padding([.top, .bottom])
+			
+			ScrollView {
+				VStack(alignment: .leading) {
+					LicenseView().padding(.bottom)
+					ContributorsView().padding(.bottom)
+					DependenciesView()
 				}
-				
-			}.frame(maxWidth: .infinity, alignment: .center)
-			.multilineTextAlignment(.center)
-			
-			Text("").padding()
-			
-			Group {
-				Text("Open source:")
-					.font(.headline)
-					.padding()
-				
-				Group {
-					Text("This project is open source. Feel free to contribute enhancements, fixes and issue tickes:")
-					
-					LinkButtonView(text: "https://github.com/philipparndt/mqtt-analyzer",
-								   url: "https://github.com/philipparndt/mqtt-analyzer")
-					LinkButtonView(text: "License",
-								   url: "https://github.com/philipparndt/mqtt-analyzer/blob/master/LICENSE")
-				}.padding(.leading)
-			}
-			
-			Text("")
-			
-			Group {
-				Text("Contributors:")
-					.font(.headline)
-					.padding()
-				
-				Group {
-					Text("Thanks for testing, contributing features and ideas.")
-					
-					LinkButtonView(text: "Ulrich Frank",
-								   url: "https://github.com/UlrichFrank")
-				}.padding(.leading)
-			}
-			
-			Text("")
-			
-			Group {
-				Text("Dependencies:")
-					.font(.headline)
-					.padding()
-			
-				Group {
-					Text("Thank you! This project would not be possible without your great work!")
-
-					LinkButtonView(text: "Moscapsule", url: "https://github.com/flightonary/Moscapsule")
-					LinkButtonView(text: "OpenSSL-Universal", url: "https://github.com/krzyzanowskim/OpenSSL")
-					LinkButtonView(text: "RealmSwift", url: "https://realm.io/docs/swift/latest/")
-					LinkButtonView(text: "IceCream", url: "https://github.com/caiyue1993/IceCream")
-					LinkButtonView(text: "Highlightr", url: "https://github.com/raspu/Highlightr")
-					LinkButtonView(text: "SwiftyJSON", url: "https://github.com/SwiftyJSON/SwiftyJSON")
-					LinkButtonView(text: "swift-petitparser", url: "https://github.com/philipparndt/swift-petitparser")
-				}.padding(.leading)
 			}
 			
 			Spacer()
@@ -103,6 +39,92 @@ struct AboutView: View {
 	
 	func close() {
 		self.isPresented = false
+	}
+}
+
+struct AboutTitleView: View {
+	var body: some View {
+		Group {
+			HStack {
+				Image("About")
+					.resizable()
+					.frame(width: 50.0, height: 50.0)
+					.cornerRadius(10)
+					.shadow(radius: 10)
+					.padding(.trailing)
+				
+				VStack(alignment: .leading) {
+					Text("MQTTAnalyzer")
+						.font(.title)
+
+					LinkButtonView(text: "© 2020 Philipp Arndt", url: "https://github.com/philipparndt")
+						.font(.caption)
+						.foregroundColor(.secondary)
+				}
+			}
+			
+		}
+		.frame(maxWidth: .infinity, alignment: .center)
+		.multilineTextAlignment(.center)
+		.padding([.top, .bottom])
+	}
+}
+
+struct LicenseView: View {
+	var body: some View {
+		Group {
+			Text("Open source:")
+				.font(.headline)
+				.padding(.bottom)
+			
+			Group {
+				Text("This project is open source. Feel free to contribute enhancements, fixes and issue tickes:")
+				
+				LinkButtonView(text: "https://github.com/philipparndt/mqtt-analyzer",
+							   url: "https://github.com/philipparndt/mqtt-analyzer")
+				LinkButtonView(text: "License",
+							   url: "https://github.com/philipparndt/mqtt-analyzer/blob/master/LICENSE")
+			}
+		}
+	}
+}
+
+struct ContributorsView: View {
+	var body: some View {
+		Group {
+			Text("Contributors:")
+				.font(.headline)
+				.padding(.bottom)
+			
+			Group {
+				Text("Thanks for testing, contributing features and ideas.")
+				
+				LinkButtonView(text: "Ulrich Frank",
+							   url: "https://github.com/UlrichFrank")
+			}
+		}
+	}
+}
+
+struct DependenciesView: View {
+	var body: some View {
+		Group {
+			Text("Dependencies:")
+				.font(.headline)
+				.padding(.bottom)
+		
+			Group {
+				Text("Thank you! This project would not be possible without your great work!")
+				
+				LinkButtonView(text: "Moscapsule", url: "https://github.com/flightonary/Moscapsule")
+				LinkButtonView(text: "OpenSSL-Universal", url: "https://github.com/krzyzanowskim/OpenSSL")
+				LinkButtonView(text: "RealmSwift", url: "https://realm.io/docs/swift/latest/")
+				LinkButtonView(text: "IceCream", url: "https://github.com/caiyue1993/IceCream")
+				LinkButtonView(text: "Highlightr", url: "https://github.com/raspu/Highlightr")
+				LinkButtonView(text: "SwiftyJSON", url: "https://github.com/SwiftyJSON/SwiftyJSON")
+				LinkButtonView(text: "swift-petitparser", url: "https://github.com/philipparndt/swift-petitparser")
+			}
+		}
 	}
 }
 
