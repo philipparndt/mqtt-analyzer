c0fa871fb8f761e1845dae5e28a23e57bb1b8f00
Philipp Arndt <2f.mail@gmx.de>
2020-03-08 14:16:45 +0100

    add script to prepare simulator for screenshots

--- src/prepare-screenshot.sh
+++ src/prepare-screenshot.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-xcrun simctl status_bar "iPhone 11" override \
+xcrun simctl status_bar "iPhone 11 Pro" override \
+--time 9:41 \
+--dataNetwork wifi \
+--wifiMode active \
+--wifiBars 3 \
+--cellularMode active \
+--cellularBars 4 \
+--batteryState charged \
+--batteryLevel 100
+
+xcrun simctl status_bar "iPhone 8" override \
+--time 9:41 \
+--dataNetwork wifi \
+--wifiMode active \
+--wifiBars 3 \
+--cellularMode active \
+--cellularBars 4 \
+--batteryState charged \
+--batteryLevel 100
+
+xcrun simctl status_bar "iPad Pro (11-inch)" override \
 --time 9:41 \
 --dataNetwork wifi \
 --wifiMode active \
