826b74fb80a67de8e9d34ace8c5e6668584200c2
Philipp Arndt <2f.mail@gmx.de>
2020-03-09 13:42:39 +0100

    -

--- src/prepare-screenshot.sh
+++ src/prepare-screenshot.sh
@@ -28,3 +28,13 @@ xcrun simctl status_bar "iPad Pro (11-inch)" override \
 --cellularBars 4 \
 --batteryState charged \
 --batteryLevel 100
+
+xcrun simctl status_bar "iPad Air (3rd generation)" override \
+--time 9:41 \
+--dataNetwork wifi \
+--wifiMode active \
+--wifiBars 3 \
+--cellularMode active \
+--cellularBars 4 \
+--batteryState charged \
+--batteryLevel 100
