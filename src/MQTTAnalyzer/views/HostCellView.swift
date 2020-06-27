66838a8114537cb2462ade784eb2ba1d72a355fd
Philipp Arndt <2f.mail@gmx.de>
2020-06-27 14:53:22 +0200

    fixes https://github.com/philipparndt/mqtt-analyzer/issues/63

--- src/MQTTAnalyzer/views/HostCellView.swift
+++ src/MQTTAnalyzer/views/HostCellView.swift
@@ -13,13 +13,26 @@ enum HostCellViewSheetType {
 	case login
 }
 
+struct ServerPageSheetState {
+	var isPresented = false
+	var type = HostCellViewSheetType.edit
+	
+	init() {
+		print("init sheet state")
+	}
+	
+	mutating func present(type: HostCellViewSheetType) {
+		self.type = type
+		self.isPresented = true
+	}
+}
+
 struct HostCellView: View {
 	@EnvironmentObject var model: RootModel
 	@ObservedObject var host: Host
 	@ObservedObject var messageModel: MessageModel
 	
-	@State var sheetPresented = false
-	@State var sheetType = HostCellViewSheetType.edit
+	@Binding var sheetState: ServerPageSheetState
 	
 	@State private var loginData = LoginData()
 	
@@ -71,16 +84,16 @@ struct HostCellView: View {
 					MenuButton(title: "Connect", systemImage: "play.circle", action: connect)
 				}
 			}
-		}.sheet(isPresented: $sheetPresented, onDismiss: cancelEditCreation, content: {
-			if self.sheetType == .login {
-				LoginDialogView(loginCallback: self.login, host: self.host, data: $loginData)
+		}.sheet(isPresented: $sheetState.isPresented, onDismiss: cancelEditCreation, content: {
+			if sheetState.type == .edit {
+				EditHostFormModalView(closeHandler: self.cancelEditCreation,
+									  root: self.model,
+									  hosts: self.model.hostsModel,
+									  original: self.host,
+									  host: transformHost(source: self.host))
 			}
 			else {
-				EditHostFormModalView(closeHandler: self.cancelEditCreation,
-					root: self.model,
-					hosts: self.model.hostsModel,
-					original: self.host,
-					host: transformHost(source: self.host))
+				LoginDialogView(loginCallback: self.login, host: self.host, data: $loginData)
 			}
 		})
 		.onAppear {
@@ -96,8 +109,7 @@ struct HostCellView: View {
 	}
 	
 	func editHost() {
-		sheetType = .edit
-		sheetPresented = true
+		sheetState.present(type: .edit)
 	}
 	
 	func disconnect() {
@@ -106,8 +118,7 @@ struct HostCellView: View {
 	
 	func connect() {
 		if self.host.needsAuth {
-			sheetType = .login
-			sheetPresented = true
+			sheetState.present(type: .login)
 		}
 		else {
 			model.connect(to: host)
@@ -115,12 +126,10 @@ struct HostCellView: View {
 	}
 	
 	func login() {
-		sheetType = .login
-		sheetPresented = false
 		model.connect(to: self.host)
 	}
 	
 	func cancelEditCreation() {
-		sheetPresented = false
+		sheetState.isPresented = false
 	}
 }
