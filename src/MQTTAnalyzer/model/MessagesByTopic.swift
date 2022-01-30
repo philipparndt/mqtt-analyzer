d6c6acca124d5b1044c54a74f8731114bc591fd0
Philipp Arndt <2f.mail@gmx.de>
2022-01-30 07:11:36 +0100

    clean-up

--- src/MQTTAnalyzer/model/MessagesByTopic.swift
+++ src/MQTTAnalyzer/model/MessagesByTopic.swift
@@ -42,32 +42,6 @@ class MessagesByTopic: Identifiable, ObservableObject {
 		messages.insert(message, at: 0)
 	}
 	
-//	func collectValues(date: Date, json: JSON, path: [String], dateFormatted: String) {
-//		json.dictionaryValue
-//		.forEach {
-//			let child = $0.value
-//			var nextPath = path
-//			nextPath += [$0.key]
-//
-//			collectValues(date: date, json: child, path: nextPath, dateFormatted: dateFormatted)
-//		}
-//
-//		let path = DiagramPath(path.joined(separator: "."))
-//		if let value = json.rawValue as? AnyHashable {
-//			self.timeSeries.put(key: path, value: TimeSeriesValue(value: value, at: date, dateFormatted: dateFormatted))
-//
-//			let val = MTimeSeriesValue(value: value, timestamp: date)
-//			if let existingValues = self.timeSeriesModels[path] {
-//				existingValues.values += [val]
-//				self.timeSeriesModels[path] = existingValues
-//			} else {
-//				let model = MTimeSeriesModel()
-//				model.values += [val]
-//				self.timeSeriesModels[path] = model
-//			}
-//		}
-//	}
-	
 	func getRecent() -> String {
 		return messages.isEmpty ? "<undef>" : messages[0].payload.dataString
 	}
@@ -75,25 +49,5 @@ class MessagesByTopic: Identifiable, ObservableObject {
 	func getRecentMessage() -> Message? {
 		return messages.isEmpty ? nil : messages[0]
 	}
-//
-//	func getDiagrams() -> [DiagramPath] {
-//		return Array(timeSeries.dict.keys).sorted { $0.path < $1.path }
-//	}
-//
-//	func hasDiagrams() -> Bool {
-//		return !timeSeries.dict.isEmpty
-//	}
-//
-//	func getTimeSeriesLastValue(_ path: DiagramPath) -> TimeSeriesValue? {
-//		let values = timeSeries.dict[path] ?? [TimeSeriesValue]()
-//		return values.last
-//	}
-//
-//	func getTimeSeries(_ path: DiagramPath) -> [TimeSeriesValue] {
-//		return timeSeries.dict[path] ?? [TimeSeriesValue]()
-//	}
-//
-//	func getTimeSeriesId(_ path: DiagramPath) -> [TimeSeriesValue] {
-//		return getTimeSeries(path)
-//	}
+
 }
