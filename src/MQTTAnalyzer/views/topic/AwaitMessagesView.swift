a86cb2fa11aba42506f71360ab120d7ef2e3259d
Philipp Arndt <2f.mail@gmx.de>
2022-02-05 11:48:01 +0100

    clean await messages background

--- src/MQTTAnalyzer/views/topic/AwaitMessagesView.swift
+++ src/MQTTAnalyzer/views/topic/AwaitMessagesView.swift
@@ -11,31 +11,38 @@ import SwiftUI
 struct AwaitMessagesView: View {
 	@ObservedObject var model: TopicTree
 	@ObservedObject var host: Host
-	
+	@Environment(\.colorScheme) var colorScheme
+
     var body: some View {
-		List {
-			TopicsToolsView(model: self.model)
+		VStack {
+			VStack(alignment: .leading) {
+				Divider()
+				TopicsToolsView(model: self.model)
+				Divider()
+			}
+			.padding([.horizontal, .top], 8)
+			.background(Color.listItemBackground(colorScheme))
+			
+			HStack {
+				if host.state == .connected {
+					ProgressView()
+					   .progressViewStyle(CircularProgressViewStyle())
+					
+					Text("Waiting for messages").padding([.leading])
+				}
+				else if host.state == .connecting {
+					ProgressView()
+					   .progressViewStyle(CircularProgressViewStyle())
+					
+					Text("Connecting").padding([.leading])
+				}
+				else {
+					Text("Not connected").padding([.leading])
+				}
+			}
+			.padding([.top], 40)
+			Spacer()
 		}
-		.listStyle(GroupedListStyle())
-		.frame(height: 200, alignment: .top)
-		
-		HStack {
-			if host.state == .connected {
-				ProgressView()
-				   .progressViewStyle(CircularProgressViewStyle())
-				
-				Text("Waiting for messages").padding([.leading])
-			}
-			else if host.state == .connecting {
-				ProgressView()
-				   .progressViewStyle(CircularProgressViewStyle())
-				
-				Text("Connecting").padding([.leading])
-			}
-			else {
-				Text("Not connected").padding([.leading])
-			}
-		}
-		Spacer()
+		.background(Color.listBackground(colorScheme))
     }
 }
