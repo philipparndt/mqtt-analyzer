2ca1889362c78e1459943fc7e4b647fccd734c1e
Philipp Arndt <2f.mail@gmx.de>
2022-03-29 19:11:24 +0200

    test old active-mq version

--- ci/start-active-mq.mjs
+++ ci/start-active-mq.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 try {
-    await $`brew install apache-activemq`
+    await $`brew install apache-activemq@5.16.4`
     await $`/usr/local/opt/activemq/bin/activemq start`
 }
 catch (e) {
