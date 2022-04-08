bec89000288f5de4cb665354f5615864e9a28772
Philipp Arndt <2f.mail@gmx.de>
2022-04-08 18:14:51 +0200

    fix test failure with new CocoaMQTT version

--- src/MQTTAnalyzerTests/CocoaMQTTRegression.swift
+++ src/MQTTAnalyzerTests/CocoaMQTTRegression.swift
@@ -11,7 +11,7 @@ import CocoaMQTT
 
 class CocoaMQTTRegressionTests: XCTestCase {
 	func testDecodeBinary789c8d() {
-		UserDefaults().set("3.1.1", forKey: "version")
+		UserDefaults().set("3.1.1", forKey: "cocoamqtt_mqtt_version")
 		
 		let publish = MqttDecodePublish()
 		publish.decodePublish(fixedHeader: 49, publishData:
